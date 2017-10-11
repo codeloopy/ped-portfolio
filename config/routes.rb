96e96ab28d28723fd3b820004cb56bdfeb56a497
Pedro <codeloopy@gmail.com>
2017-10-11 19:12:07 -0400

    customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  ##to change the way the resources routes show up
+  # resources :portfolios, except: [:show]
+  # get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :portfolios
 
   get 'about', to: "pages#about"
