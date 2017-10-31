591fb25dd09e119e0080032afca6f7af74de1a81
Pedro <codeloopy@gmail.com>
2017-10-30 22:40:21 -0400

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+
   ##to change the way the resources routes show up
   # resources :portfolios, except: [:show]
   # get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
+  devise_for :users
   resources :portfolios
 
   get 'about', to: "pages#about"
