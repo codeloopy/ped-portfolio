45d0e8f74fa8f5f30383cb0a7518fb0c49c93fa2
Pedro <codeloopy@gmail.com>
2017-11-28 23:06:58 -0500

    tweaked application controller

--- Gemfile
+++ Gemfile
@@ -47,3 +47,4 @@ gem 'friendly_id', '~> 5.1.0' # Note: You MUST use 5.0.0 or greater for Rails 4.
 gem 'devise', '~> 4.3'
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 gem 'jquery-rails'
+gem 'petergate', '~> 1.8'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,19 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = PedsViewTool::Renderer.copyright('Pedro Monsalve', 'All Rights Reserved')
+  end
+
+  module PedsViewTool
+    class Renderer
+      def self.copyright(name, msg)
+        "&copy; #{Time.now.year} | <strong>#{name}, #{msg}</strong>".html_safe
+      end
+    end
+  end
+
 
 end
