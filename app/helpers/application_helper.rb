81bbfaf463d718bfd18f3d2ead6914ee1def9bd4
Pedro <codeloopy@gmail.com>
2017-12-01 01:00:52 -0500

    styled and implemented partial for nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-  def login_logic_helper
+  def login_logic_helper(style)
     if current_user.is_a?(GuestUser)
-      reg = link_to 'Register', new_user_registration_path
-      login = link_to 'Login', new_user_session_path
+      reg = link_to 'Register', new_user_registration_path, class: style
+      login = link_to 'Login', new_user_session_path, class: style
       "#{reg} #{login}".html_safe
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
