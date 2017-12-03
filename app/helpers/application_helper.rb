67ed7446700f36b65db00043fee1c698790454d1
Pedro <codeloopy@gmail.com>
2017-12-02 22:02:32 -0500

    Update devise style for user edit page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,10 +10,6 @@ module ApplicationHelper
     end
   end
 
-  def sample_helper
-    content_tag(:div, "My content", class: "my-class")
-  end
-
   def source_helper(layout_name)
     if session[:source]
       greeting = "Thanks for visiting me from #{session[:source]}, you are on the #{layout_name} layout"
