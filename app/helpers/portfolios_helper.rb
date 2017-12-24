d743d16456c8aa5676baaa2351291232386503e4
Pedro <codeloopy@gmail.com>
2017-12-24 01:33:21 -0500

    updated file upload form and refactored placeholder funtionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,16 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+
+  def portfolio_img(img, type)
+    # byebug
+    if img.model.main_image? || img.model.thumb_image?
+      img
+    elsif type == 'main'
+      image_generator(height: '600', width: '400')
+    else
+      image_generator(height: '350', width: '200')
+    end
+  end
 end
