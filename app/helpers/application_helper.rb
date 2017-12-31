0dd22298c0532f6c31081bec233621799eb36b22
Pedro <codeloopy@gmail.com>
2017-12-30 22:26:09 -0500

    Refactored gitter alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,4 +56,14 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    
+    alert_generator(alert) if alert
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, sticky: false)
+  end
+
 end
